c21f6137b1a06dc8f797b947d0051c55096a82d6
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-11-22 21:38:29 +0500

    nvbr

---
+++ components/Navbar.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+function Navbar() {
+  return (
+    /*Navbar/4/ */ <nav className="w-[428px] h-[72px]">
+      {/* Container*/}
+      <div className="bg-[#FFFFFF] w-[428px] h-[72px] p-[24px] border-b-[1px]">
+        {/* Container*/}
+        <div className="w-[380px] h-[48px] flex ml-[32px]">
+          {/* Column left*/}
+          <div className="w-[300px] h-[41px]">
+            {/* Ddsgnr Lib*/}{" "}
+            <div className="px-[29] w-[130.6px] h-[41px]">
+              {/* Ddsgnr Themes*/}
+              <div className="w-[130.6px] h-[30.38px] flex gap-[8.02px]  ">
+                <div className="w-[32.58px] h-[30.38px]">
+                  {" "}
+                  <svg
+                    width="33"
+                    height="32"
+                    viewBox="0 0 33 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.7929 4.59521V4.59626C15.776 4.62534 12.2955 10.6292 7.95067 17.9595C3.59532 25.3077 0 31.1906 0 31.1906L12.3315 31.1365L12.3323 31.1364L12.3297 31.1459C18.3172 31.03 23.7182 27.0934 25.3119 21.1453C25.8102 19.286 25.8853 17.4143 25.5964 15.6255C24.8043 10.3582 20.9149 6.07448 15.7929 4.59521Z"
+                      fill="black"
+                    />
+                    <path
+                      d="M22.6077 0.809326V0.81037C22.5908 0.839449 19.1102 6.8433 14.7654 14.1736C10.4101 21.5218 6.81476 27.4047 6.81476 27.4047L19.1462 27.3506L19.147 27.3505L19.1445 27.36C25.132 27.2441 30.5329 23.3075 32.1267 17.3594C32.6249 15.5001 32.7001 13.6284 32.4111 11.8396C31.619 6.57236 27.7297 2.28859 22.6077 0.809326Z"
+                      fill="#10F2C5"
+                    />
+                  </svg>
+                </div>
+                <div className="text-[#000000;] font-bold w-[90px] h-[30px] ">
+                  Ddsgnr
+                </div>
+              </div>
+              {/* Ddsgnr Themes*/}
+            </div>
+            {/* Ddsgnr Lib*/}
+          </div>
+          {/* Column left*/}
+
+          {/* Column right*/}
+          <div className="pt-[7px] ">
+          <div className=" pb-[18px]">
+            
+            <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" fill="black"/>
+</svg>
+</div></div>
+          {/* Column right*/}
+        
+        
+        </div>
+        {/* Container*/}
+        </div>
+      {/* Container*/}
+    </nav> /*Navbar/4/ */
+  );
+}
+
+export default Navbar;
